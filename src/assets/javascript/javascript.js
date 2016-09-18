2d666e29899676100103f8d10d6eb5989525dd96
Julien Brionne <julien.brionne@gmail.com>
2016-09-19 00:14:22 +0200

    Advance

--- src/assets/javascript/javascript.js
+++ src/assets/javascript/javascript.js
@@ -27,22 +27,22 @@ window.onload = function() {
     // Get Instagram photos
     var token = '1241846.5a75dba.762408d8526b4652939cd2f53ffd50d6',
     num_photos = 8;
- 
-$.ajax({
-    url: 'https://api.instagram.com/v1/users/self/media/recent',
-    dataType: 'jsonp',
-    type: 'GET',
-    data: {access_token: token, count: num_photos},
-    success: function(data){
-        console.log(data);
-        for( x in data.data ){
-            $('ul.insta').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>');
+     
+    $.ajax({
+        url: 'https://api.instagram.com/v1/users/self/media/recent',
+        dataType: 'jsonp',
+        type: 'GET',
+        data: {access_token: token, count: num_photos},
+        success: function(data){
+            console.log(data);
+            for( x in data.data ){
+                $('ul.insta').append('<li><figure class="roll"><a href="'+data.data[x].link+'"><img src="'+data.data[x].images.standard_resolution.url+'"><figcaption><ul><li><i class="fa fa-heart"></i> '+data.data[x].likes.count+'</li><li><i class="fa fa-comment"></i> '+data.data[x].comments.count+'</li></ul></figcaption></a></figure></li>');
+            }
+        },
+        error: function(data){
+            console.log(data);
         }
-    },
-    error: function(data){
-        console.log(data);
-    }
-});
+    });
 
 
     // Open Twitter/share in a Pop-Up
